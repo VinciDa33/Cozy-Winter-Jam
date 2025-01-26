e17c39a5c363f8c871678db12bc27cd4dfb37283
VinciDa <marcusbthomsen@gmail.com>
2025-01-26 16:35:23 +0100

    Arh

--- CozyWinterJam/Assets/LevelButtons.cs
+++ CozyWinterJam/Assets/LevelButtons.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class LevelButtons : MonoBehaviour
 {
     public void SkipLevel()
@@ -14,5 +14,11 @@ public class LevelButtons : MonoBehaviour
     public void Exit()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
